2eac9166fe34bcd8a092bf3b8582c985d6f0e54c
Richard <weizhe_2008@aliyun.com>
2014-11-17 17:03:10 +0800

    compress png with zoom enhanced

--- src/js/app.harley-1.0.0.js
+++ src/js/app.harley-1.0.0.js
@@ -12,33 +12,49 @@
 
     $(document).ready(function() {
 
-        $(".zoom-control").fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000);
-        $(".scroll-hint").fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000);
+        $(".zoom-control").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500);
+        $(".scroll-hint").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500);
 
-
-        $(".view-panel").bind("click", function() {
-            var control = $($(this).children(".zoom-control")[0]);
-            if (control.css("background-image").indexOf("zoom-in") > 0) {
-                control.css("background-image", "url(images/zoom-out.png)");
-                $("#zoom-view").removeClass("zoom-view-in").addClass("zoom-view-out");
-                $(".zoom-control").fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000);
-            } else {
-                control.css("background-image", "url(images/zoom-in.png)");
-                $("#zoom-view").removeClass("zoom-view-out").addClass("zoom-view-in");
-            }
-        });
-        var imgload = function (img, callback) {
-            var timer = setInterval(function() {
-                if (img.complete) {
-                    callback(img)
-                    clearInterval(timer)
+        var zoom = function() {
+            return (function() {
+                var zoom = $(".zoom-control");
+                var back = $(".back-control");
+                var view = $(".harley-zoom-view");
+                var slider = $(".harley-description-panel");
+                if (zoom.css("background-image").indexOf("zoom-in") > 0) {
+                    zoom.css("background-image", "url(images/zoom-out.png)");
+                    view.animate({opacity: 0}, 500, function() {
+                        $(this).removeClass("zoom-view-in").addClass("zoom-view-out");
+                        $(this).css("background-image", "url(images/harley01/harley-f.png)");
+                    }).animate({opacity: 1}, 500);
+//                    view.css("background-image", "url(images/harley01/harley-f.png)");
+                    zoom.fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500);
+                    slider.fadeOut();
+                    back.fadeOut();
+                } else {
+                    zoom.css("background-image", "url(images/zoom-in.png)");
+                    view.animate({opacity: 0}, 500, function() {
+                        $(this).removeClass("zoom-view-out").addClass("zoom-view-in");
+                        $(this).css("background-image", "url(images/harley01/harley-c.png)");
+                    }).animate({opacity: 1}, 500);
+                    slider.fadeIn();
+                    back.fadeIn();
                 }
-            }, 50)
+            })();
         };
-        var fadeIn = function(that){
-            $(that).fadeIn("slow");
-            $(that).attr("width", "100%");
-        };
+        $(".harley-fill").addSwipeEvents().
+            bind('doubletap', function(evt, touch) {
+                // triggered for swipe events
+                zoom();
+        });
+        $(".harley-zoom-view").addSwipeEvents().
+            bind('doubletap', function(evt, touch) {
+                // triggered for swipe events
+                zoom();
+            });
+        $(".harley-fill").bind("dblclick", function() {
+            zoom();
+        });
 
 
     });
--- src/js/my-app.js
+++ src/js/my-app.js
@@ -82,15 +82,45 @@ var contentEffect = function() {
     $(".scroll-hint").fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000);
 
 
-    $(".view-panel").unbind("click").bind("click", function() {
-        var control = $($(this).children(".zoom-control")[0]);
-        if (control.css("background-image").indexOf("zoom-in") > 0) {
-            control.css("background-image", "url(images/zoom-out.png)");
-            $("#zoom-view").removeClass("zoom-view-in").addClass("zoom-view-out");
-            $(".zoom-control").fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000);
-        } else {
-            control.css("background-image", "url(images/zoom-in.png)");
-            $("#zoom-view").removeClass("zoom-view-out").addClass("zoom-view-in");
-        }
+    var zoom = function() {
+        return (function() {
+            var zoom = $(".zoom-control");
+            var back = $(".back-control");
+            var view = $(".harley-zoom-view");
+            var slider = $(".harley-description-panel");
+            if (zoom.css("background-image").indexOf("zoom-in") > 0) {
+                zoom.css("background-image", "url(images/zoom-out.png)");
+                view.animate({opacity: 0}, 500, function() {
+                    $(this).removeClass("zoom-view-in").addClass("zoom-view-out");
+                    $(this).css("background-image", "url(images/harley01/harley-f.png)");
+                }).animate({opacity: 1}, 500);
+//                    view.css("background-image", "url(images/harley01/harley-f.png)");
+                zoom.fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500);
+                slider.fadeOut();
+                back.fadeOut();
+            } else {
+                zoom.css("background-image", "url(images/zoom-in.png)");
+                view.animate({opacity: 0}, 500, function() {
+                    $(this).removeClass("zoom-view-out").addClass("zoom-view-in");
+                    $(this).css("background-image", "url(images/harley01/harley-c.png)");
+                }).animate({opacity: 1}, 500);
+                slider.fadeIn();
+                back.fadeIn();
+            }
+        })();
+    };
+
+    $(".harley-fill").addSwipeEvents().
+        bind('doubletap', function(evt, touch) {
+            // triggered for swipe events
+            zoom();
+        });
+    $(".harley-zoom-view").addSwipeEvents().
+        bind('doubletap', function(evt, touch) {
+            // triggered for swipe events
+            zoom();
+        });
+    $(".harley-fill").bind("dblclick", function() {
+        zoom();
     });
 }
